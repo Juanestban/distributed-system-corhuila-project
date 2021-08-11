2800dd3b782e2ed6cefa9350973a630e75b7725b
Juan Esteban <juanestban000@gmail.com>
2021-08-10 19:36:23 -0500

    Feat: installing the morgan and types for can support this module and initializer the app express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-console.log('hello world')
-console.log('testing lint-staged')
+import express, { Express } from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+// Initializer
+const app: Express = express()
+
+// Settings
+app.set('port', process.env.PORT || 3200)
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
+
+// Routes
+
+// Starting server
+app.listen(app.get('port'), () => {
+  console.log('Mangar-Sword | [+] listen on port:', app.get('port'))
+})
